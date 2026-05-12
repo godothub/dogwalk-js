61abec868a291b913f8560d292860daeacc18d22
moluopro <moluopro@qq.com>
2026-05-12 21:59:49 +0800

    Only show launch logos in game scene

--- source/globals/launch.gd
+++ source/globals/launch.gd
@@ -1,4 +1,6 @@
 extends Node
 
 func _ready() -> void:
+	if not get_tree().current_scene or get_tree().current_scene.name != "Game":
+		return
 	get_tree().current_scene.add_child(preload("res://source/user_interface/launch_logos.tscn").instantiate())
